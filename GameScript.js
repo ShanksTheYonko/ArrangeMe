9a8f9024c32d5346b61ae24cc65f1fd26a36e7ff
Monkey D Luffy <yah68863@laoeq.com>
2016-06-13 15:00:08 +0300

    What i did so far

--- GameScript.js
+++ GameScript.js
@@ -5,7 +5,7 @@ function GameBoard(rows, cols){
 	var empty = cellsNumber;
 
 
-	this.initCells = function(cellsNumber){
+	this.initCells = function(){
 		for (var i = 1; i <= cellsNumber - 1; i++){
 			this[i] = i;
 		}
@@ -17,7 +17,9 @@ function GameBoard(rows, cols){
 			this.cells[empty] = this.cells[index];
 			this.cells[index] = -1;
 			empty = index;
+			return temp;
 		}
+		return index;
 	};
 
 	this.getPuzzlePartAt = function(index){
@@ -35,7 +37,7 @@ function GameBoard(rows, cols){
 		return true;
 	};
 
-	this.cells = new this.initCells(cellsNumber);
+	this.cells = new this.initCells();
 };
 
 
@@ -47,13 +49,17 @@ function GameConfig(version, tableHeight, tableWidth){
 	var tableWidth = tableWidth;
 	var imgHeight = 0;
 	var imgWidth = 0;
+	var rows = 0;
+	var cols = 0;
 	this.initConfig = function(){
-		sizes = {"3x3" : 3.0};
+		sizes = {"3x3" : 3};
 		size = sizes[version];
 		imgHeight = tableHeight / size;
 		imgWidth = tableWidth / size;
 		baseFolder = version + "/";
 		fileType = ".png";
+		rows = size;
+		cols = size;
 	}
 	this.getImgHeight = function(){
 		return imgHeight;
@@ -63,6 +69,12 @@ function GameConfig(version, tableHeight, tableWidth){
 	};
 	this.getBaseFolder = function(){
 		return baseFolder;
+	};
+	this.getRows = function(){
+		return rows;
+	}
+	this.getCols = function(){
+		return cols;
 	}
 	this.getPathOf = function(index){
 		return baseFolder + index + fileType;
@@ -71,5 +83,46 @@ function GameConfig(version, tableHeight, tableWidth){
 }
 
 
-var example = new GameBoard(3,3);
+function Game(config){
+	var config = config;
+	var board = new GameBoard(config.getRows(), config.getCols());
+	this.check = function(){
+		var result = board.check();
+		if (result){
+			alert("Win");
+		} else {
+			alert("Lose");
+		}
+	};
 
+	var movePuzzlePart = function(index){
+		oldID = "img" + index
+		newID = "img" + index
+		var newLocation = board.movePuzzlePart(index);
+		var temp = document.getElementById(oldID).src;
+		document.getElementById(oldID).src = "";
+		document.getElementById(newID) = temp;
+	};
+	
+	this.initilize = function(objName){
+		board.initCells();
+		var rows = config.getRows();
+		var cols = config.getCols();
+		var cells = rows * cols;
+		for (var puzzlePart = 1; puzzlePart <= cells; puzzlePart++){
+			var imgIndex = board.getPuzzlePartAt(puzzlePart);
+			if (imgIndex != -1){
+				var id = "img" + imgIndex;
+				document.getElementById(id).src = config.getPathOf(imgIndex);
+				document.getElementById(id).addEventListener( 'click', function(){movePuzzlePart(puzzlePart)}, false);
+			}
+		}
+	};
+
+	
+};
+
+
+
+var config = new GameConfig("3x3", 400, 400);
+var game = new Game(config);
